2396294f8abfd423ffcae22ec64b1ee7bf983e29
StewieTech <ErrolF.Stewart@gmail.com>
2024-07-01 15:23:20 -0400

    Updating Hashmap

--- Project/Compiler/bptn-compiler/src/main/java/Compiler/Lexical.java
+++ Project/Compiler/bptn-compiler/src/main/java/Compiler/Lexical.java
@@ -2,6 +2,9 @@ package Compiler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
 
 /* 
  * Updating this function to use HashMaps instead of ArrayLists
@@ -11,22 +14,56 @@ import java.util.Map;
 public class Lexical {
   // I want to ingest the user's input and spit it out into tokens
   private String userInput; // should this be constant ?
-  private int keywordsIndex;
-  private Map<TokenType, List<Token>> tokensMap;
-  private Map<TokenType, Integer> tokenCount;
-  // List<String> keywords = new ArrayList<>(); // should this be immutable ?
+  private int positionIndex;
+  private Map<TokenType, List<Tokens>> tokensMap;
+  private Map<TokenType, Integer> tokenCounter;
 
   public Lexical(String userInput) {
     this.userInput = userInput;
+    this.positionIndex = 0;
+    this.tokensMap = new HashMap<>();
+    this.tokenCounter = new HashMap<>();
+    for (TokenType type : TokenType.values()) {
+      tokensMap.put(type, new ArrayList<>());
+      tokenCounter.put(type, 0);
+    }
+  }
 
-    // this.userInputLength = userInput.lenth(); // may not need this
+  // Here I am just looking at the character
+  private char peek() {
+    return userInput.charAt(positionIndex);
+  }
+
+  // I want to injest the character then advance the position one over
+  private boolean isSeparator(char character) {
+    return "{}(),;".indexOf(character) != -1;
+  }
+
+  private boolean isOperator(char character) {
+    return "+-*/|&!=%<>".indexOf(character) != -1;
+  }
+
+  private boolean isKeyword(String element) {
+    return Set.of("if", "else", "return", "for", "while").contains(element);
+  }
+
+  private char consume() {
+    return userInput.charAt(positionIndex++);
+  }
+
+  private Tokens consumeNumber() {
+    StringBuilder numberString = new StringBuilder();
+    while (positionIndex < userInput.length() && Character.isDigit(peek())) {
+      numberString.append(consume());
+    }
+    return addTokens(TokenType.NUMBER, numberString.toString());
   }
 
   // I am going to be returning a tokentype based on what the character is
   public List<TokenType> tokenize() {
     List<TokenType> tokenList = new ArrayList<>();
-		while (keywordsIndex < userInput.length()) {
-     char currentCharacter = userInput.charAt(keywordsIndex);
+    while (keywordsIndex < userInput.length()) {
+      char currentCharacter = userInput.charAt(keywordsIndex);
 
     if (Character.isLetter(currentCharacter)) {
       tokenList.add(consumeIdentiferOrKeyword());
@@ -52,7 +89,4 @@ public class Lexical {
     }
   }
 
-  public consumeNumber() {
-    StringBuilder numberString = new StringBuilder() 
-  }
 }
